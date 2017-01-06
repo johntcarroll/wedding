552a2fa6b756488f0f91ce5f9d2128e307cae7bf
John Carroll <john.carroll@ishb.us>
2017-01-06 09:48:35 -0500

    removed debugging after changing php include path on server

--- home/index.php
+++ home/index.php
@@ -1,11 +1,11 @@
 <?php
 
 $title = "John and Lena's Wedding Website";
-include "//layout/header.php";
+include "/layout/header.php";
 ?>
 
 John and Lena
 
 <?php
 
-include "//layout/footer.php";
+include "/layout/footer.php";
