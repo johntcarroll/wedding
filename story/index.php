4b28ad86a732de6001439dd34658c64d0a1ccfa3
John Carroll <john.carroll@ishb.us>
2017-01-06 21:06:38 -0500

    responsive fixes

--- story/index.php
+++ story/index.php
@@ -11,19 +11,22 @@ include "../layout/header.php";
     </div>
 
     <div class='row hidden-md hidden-lg'>
-        <div class='col-sm-4 col-xs-4 vcenter'>
+        <div class='col-sm-4 col-xs-12 vcenter'>
             <img class='img img-responsive' src='../img/circular-image.png' />
         </div><!--
-     --><div class='col-sm-8 col-xs-8 vcenter'>
+     --><div class='col-sm-8 col-xs-12 vcenter'>
             Hi everyone! Thanks for visiting our wedding website! A few fun facts about me: I own my own business called ISHB (shameless plug - <a href='http://ishb.us' target='_blank'> ishb.us</a>). I graduated from Drexel University in 2013 with a degree in IT and a database concentration. I had a crush on Lena in our 8th grade English class! 
         </div>
     </div>
     <div class='row hidden-md hidden-lg pad-top-20'>
-        <div class='col-sm-8 col-xs-8 vcenter'>
+        <div class='col-sm-8 hidden-xs vcenter'>
             Welcome! Little ol’ me in a nutshell: I was born in Tallahassee, FL and lived in Denver, CO while growing up before moving to PA. I went to school for biology at Kansas State University and now work in the clinical research industry for a CRO.  I enjoy Eagle’s Sundays with John and dreaming up ideas for our 1860’s fixer-upper home in the West Chester Borough.  You can find me crafting, cooking, playing soccer or beating John at Hearthstone.
         </div><!--
-     --><div class='col-sm-4 col-xs-4 vcenter'>
+     --><div class='col-sm-4 col-xs-12 vcenter'>
             <img class='img img-responsive' src='../img/circular-image.png' />
+        </div><!--
+      --><div class='col-sm-8 visible-xs vcenter'>
+            Welcome! Little ol’ me in a nutshell: I was born in Tallahassee, FL and lived in Denver, CO while growing up before moving to PA. I went to school for biology at Kansas State University and now work in the clinical research industry for a CRO.  I enjoy Eagle’s Sundays with John and dreaming up ideas for our 1860’s fixer-upper home in the West Chester Borough.  You can find me crafting, cooking, playing soccer or beating John at Hearthstone.
         </div>
     </div>
 
@@ -46,7 +49,7 @@ include "../layout/header.php";
         <div class='col-md-3 col-sm-3 col-xs-12 vcenter'>
             <img class='img img-responsive img-center' src="../img/poloriod.jpg" />
         </div><!--
-     --><div class='col-md-8 col-lg-8 col-sm-8 col-xs-12 vcenter'>
+     --><div class='col-md-8 col-lg-8 col-sm-8 col-xs-12 vcenter text-center'>
             <h1>The Beginnings</h1><br/><br/>
             Our story began at Rustin High School circa 2006 in West Chester, PA.  We were both captains of the soccer teams and irrefutably flirtatious.  While remaining friends throughout the years, we would see each other on our holiday breaks from college to enjoy time watching meteor showers, playing Jeopardy and competing for most correct answers, or spending New Year’s Eve together in Philadelphia (which remains our favorite adventurous holiday).  After college, Lena returned to West Chester.  On her August birthday of 2012, John took Lena to Teikoku for sushi.  Upon reconnecting in the same time zone, we began our deeper relationship based off the solid friendship foundation built over the years.
         </div>
