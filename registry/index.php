c44b8e25a86430ed5f52494d58fc82e29719c74c
John Carroll <john.carroll@ishb.us>
2017-04-07 16:49:36 -0400

    reg change

--- registry/index.php
+++ registry/index.php
@@ -21,7 +21,7 @@ include "../layout/header.php";
         <a target="_blank" href='https://www.kohls.com/upgrade/gift_registry/kohlsgrw_home.jsp?section=list&listid=3298378&viewtype=listview&isfromsearch=true&fname=lena&lname=rohleder&regtype=registry.wedding,registry.baby,registry.splday'><img class='img img-responsive img-center accomm-img' src='../img/JL_Registry_1440x1440_001.png'></a>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4">
-        <a target="_blank" href='https://www.zola.com/search/wedding-registry'><img class='img img-responsive img-center accomm-img' src='../img/JL_Registry_1440x1440_004.png'></a>
+        <a target="_blank" href='https://www.zola.com/registry/johnandlena'><img class='img img-responsive img-center accomm-img' src='../img/JL_Registry_1440x1440_004.png'></a>
     </div>
 </div>
 <?php
