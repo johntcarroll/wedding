f5ed1bcbec683539636056351087df39923a0507
John <john@John-Carroll.home>
2017-07-06 00:17:18 -0400

    bug fix

--- rsvp/ajax/get_guests_table.php
+++ rsvp/ajax/get_guests_table.php
@@ -25,18 +25,13 @@ for ($count = 1; $count <= $guests; $count++) {
     $line_template .= "
         <div class='guest-container'>
             <div class='row'>
-                <div class='col-xs-12'>
-                    <h4>Guest #" . $count . "</h4>
-                </div>
-            </div>
-            <div class='row'>
                 <div class='col-xs-12 col-sm-6 col-md-6 col-lg-3'>
                     <label>Guest #" . $count . " First Name</label>
                     <input class='form-control required' type='text' placeholder='First Name' name='fname[]' value='" . ($count == 1 ? $fname : '') . "' />
                 </div>
                 <div class='col-xs-12 col-sm-6 col-md-6 col-lg-3'>
                     <label>Guest #" . $count . " Last Name</label>
-                    <input class='form-control required' type='text' placeholder='Last Name' name='lname[]' value='" . ($count == 1 ? $lname : '') . "/>
+                    <input class='form-control required' type='text' placeholder='Last Name' name='lname[]' value='" . ($count == 1 ? $lname : '') . "' />
                 </div>
                 <div class='col-xs-12 col-sm-12 col-md-12 col-lg-3'>
                     <label>Guest #" . $count . " Dinner Option</label>";
