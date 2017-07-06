5487232930a54ea10d4c373818ce2995619e67d3
John <john@John-Carroll.home>
2017-07-05 22:11:46 -0400

    added rsvp page

---
+++ rsvp/index.php
@@ -0,0 +1,13 @@
+<?php
+$root = "../";
+$title = "John and Lena's Wedding Website";
+include "../layout/header.php";
+?>
+<div class='row'>
+    <div class='col-xs-12 other-bg'>
+        <h1>rsvp</h1>
+    </div>
+</div>
+
+<?php
+include "../layout/footer.php";
