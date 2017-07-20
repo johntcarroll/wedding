9f7b631cb422ff93739084879cc209625b96afe4
John <john@John-Carroll.home>
2017-07-20 09:34:53 -0400

    hid google tracking

--- layout/header.php
+++ layout/header.php
@@ -19,15 +19,7 @@
         <script type='text/javascript' src='<?php echo $root; ?>js/doubleTapBugFix.js'></script>
     </head>
     <body>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-96898973-1', 'auto');
-            ga('send', 'pageview');
-        </script>
+        <?php include 'googleTracking.php';?>
         <div class='container-fluid' style="overflow: hidden;">
             <div id="fullscreen" class='row fullscreen'>
                 <div class='col-xs-12 overlay-header'>
